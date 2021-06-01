9472fe4a52f65cda2cb346764275f00abe2d78c2
christopher simmons <kadensdad93@gmail.com>
2021-06-01 13:22:10 -0700

    updated scaling

--- cogs/crownunlimited.py
+++ cogs/crownunlimited.py
@@ -2797,8 +2797,8 @@ class CrownUnlimited(commands.Cog):
             t_stamina = t['STAM']
             t_max_stamina= t['STAM']
             t_moveset = t['MOVESET']
-            t_attack = t['ATK'] + (20 * currentopponent) + opponent_scaling
-            t_defense = t['DEF'] + (15 * currentopponent) + opponent_scaling
+            t_attack = t['ATK'] + (20 * currentopponent) + opponent_scaling + 40
+            t_defense = t['DEF'] + (15 * currentopponent) + opponent_scaling + 50
             t_type = t['TYPE']
             t_accuracy = t['ACC']
             t_passive = t['PASS'][0]
@@ -8007,8 +8007,8 @@ class CrownUnlimited(commands.Cog):
             t_stamina = t['STAM']
             t_max_stamina= t['STAM']
             t_moveset = t['MOVESET']
-            t_attack = t['ATK'] + (15 * currentopponent) + opponent_scaling
-            t_defense = t['DEF'] + (10 * currentopponent) + opponent_scaling
+            t_attack = t['ATK'] + (15 * currentopponent) + opponent_scaling + 40
+            t_defense = t['DEF'] + (10 * currentopponent) + opponent_scaling + 50
             t_type = t['TYPE']
             t_accuracy = t['ACC']
             t_passive = t['PASS'][0]
