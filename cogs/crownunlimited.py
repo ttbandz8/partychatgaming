2982a1ef27862aee7266696b5e12e54fb8556903
RocBambino <83316065+RocBambino@users.noreply.github.com>
2021-06-01 15:41:52 -0400

    damage update

--- cogs/crownunlimited.py
+++ cogs/crownunlimited.py
@@ -7967,8 +7967,8 @@ class CrownUnlimited(commands.Cog):
             t_card = t['NAME']
             t_card_path=t['PATH']
             t_rcard_path=t['RPATH']
-            t_max_health = t['HLT'] + (100 * currentopponent) + opponent_scaling
-            t_health = t['HLT'] + (100 * currentopponent) + opponent_scaling
+            t_max_health = t['HLT'] + (100 * currentopponent) + opponent_scaling + 500
+            t_health = t['HLT'] + (100 * currentopponent) + opponent_scaling + 500
             t_stamina = t['STAM']
             t_max_stamina= t['STAM']
             t_moveset = t['MOVESET']
@@ -14018,7 +14018,7 @@ def damage_cal(card, ability, attack, defense, op_defense, vul, accuracy, stamin
         defensepower = op_defense
         dmg = ((attackpower * (100 * (100 / defensepower))) * .001) + int(ap)
         # dmg = (int(ap)*(100/(100+int(op_defense)))) + int(atk)
-        low = dmg - (dmg * .50)
+        low = dmg - (dmg * .45)
         high = dmg + (dmg * .05)
 
         true_dmg = random.randint(int(low), int(high))
