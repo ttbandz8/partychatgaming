306c50d50bc3cd4345a0a8f604e18f555f158747
christopher simmons <kadensdad93@gmail.com>
2021-06-06 16:30:00 -0700

    dailybonus added

--- bot.py
+++ bot.py
@@ -317,6 +317,11 @@ async def fix(ctx, user: User):
    else:
       print(m.ADMIN_ONLY_COMMAND)
 
+@bot.command(pass_context=True)
+@commands.cooldown(1, 60*60*24, commands.BucketType.user)
+async def dailybonus(ctx):
+   await bless(100, ctx.author)
+   await ctx.send(f"Daily bonus :coin:100 has been applied for {ctx.author.mention}!")
 
 @bot.command()
 @commands.check(validate_user)
