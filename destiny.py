f045203a77c3b269054d0490acd663d228ae34df
RocBambino <83316065+RocBambino@users.noreply.github.com>
2021-06-18 22:49:22 -0400

    added attack on Titan Destinies

--- destiny.py
+++ destiny.py
@@ -10,6 +10,76 @@ destiny = [
         "UNIVERSE": "Naruto"
     },
     {
+        "NAME": "Eternal Lovers",
+        "WINS": 0,
+        "REQUIRED": 10,
+        "DEFEAT": "Founding Titan",
+        "USE_CARDS": ["Mikasa"],
+        "EARN": "Founding Titan",
+        "COMPLETED": False,
+        "UNIVERSE": "7ds"
+    },
+    {
+        "NAME": "Declaration Of War",
+        "WINS": 0,
+        "REQUIRED": 10,
+        "DEFEAT": "Jaw Titan",
+        "USE_CARDS": ["Attack Titan"], 
+        "EARN": "Warhammer Titan",
+        "COMPLETED": False,
+        "UNIVERSE": "Attack On Titan"
+    },
+    {
+        "NAME": "A New Commander",
+        "WINS": 0,
+        "REQUIRED": 10,
+        "DEFEAT": "Colossal Titan",
+        "USE_CARDS": ["Captain Erwin"],
+        "EARN": "Colossal Titan",
+        "COMPLETED": False,
+        "UNIVERSE": "Attack On Titan"
+    },
+    {
+        "NAME": "Reveal The Coordinate",
+        "WINS": 0,
+        "REQUIRED": 10,
+        "DEFEAT": "Armored Titan",
+        "USE_CARDS": ["Eren Jaeger"],
+        "EARN": "Attack Titan",
+        "COMPLETED": False,
+        "UNIVERSE": "Attack On Titan"
+    },
+    {
+        "NAME": "Among Us",
+        "WINS": 0,
+        "REQUIRED": 10,
+        "DEFEAT": "Female Titan",
+        "USE_CARDS": ["Captain Levi"],
+        "EARN": "Female Titan",
+        "COMPLETED": False,
+        "UNIVERSE": "Attack On Titan"
+    },
+    {
+        "NAME": "Dueling Demon Princes",
+        "WINS": 0,
+        "REQUIRED": 10,
+        "DEFEAT": "Zeldris",
+        "USE_CARDS": ["Meliodas"],
+        "EARN": "Zeldris",
+        "COMPLETED": False,
+        "UNIVERSE": "7ds"
+    },
+    {
+        "NAME": "Sunshine",
+        "WINS": 0,
+        "REQUIRED": 10,
+        "DEFEAT": "Escanor",
+        "USE_CARDS": ["Meliodas"],
+        "EARN": "Escanor",
+        "COMPLETED": False,
+        "UNIVERSE": "7ds"
+    },
+    {
         "NAME": "Gowthers Sin",
         "WINS": 0,
         "REQUIRED": 10,
