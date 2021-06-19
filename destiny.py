a1f4dc233fed5c4240fe7db7439d3c2545cfa997
RocBambino <83316065+RocBambino@users.noreply.github.com>
2021-06-18 21:44:20 -0400

    added some destinies

--- destiny.py
+++ destiny.py
@@ -8,5 +8,55 @@ destiny = [
         "EARN": "Awakened Itachi",
         "COMPLETED": False,
         "UNIVERSE": "Naruto"
-    }
+    },
+    {
+        "NAME": "Gowthers Sin",
+        "WINS": 0,
+        "REQUIRED": 10,
+        "DEFEAT": "Estarossa",
+        "USE_CARDS": ["Gowther"],
+        "EARN": "Estarossa",
+        "COMPLETED": False,
+        "UNIVERSE": "7ds"
+    },
+    {
+        "NAME": "True Sonshine",
+        "WINS": 0,
+        "REQUIRED": 10,
+        "DEFEAT": "Escanor",
+        "USE_CARDS": ["Estarossa"],
+        "EARN": "Mael",
+        "COMPLETED": False,
+        "UNIVERSE": "7ds"
+    },
+    {
+        "NAME": "Charizard Evolution",
+        "WINS": 0,
+        "REQUIRED": 10,
+        "DEFEAT": "Charizard",
+        "USE_CARDS": ["Charmander"],
+        "EARN": "Charizard",
+        "COMPLETED": False,
+        "UNIVERSE": "Kanto Region"
+    },
+    {
+        "NAME": "Blastoise Evolution",
+        "WINS": 0,
+        "REQUIRED": 10,
+        "DEFEAT": "Blastoise",
+        "USE_CARDS": ["Squirtle"],
+        "EARN": "Blastoise",
+        "COMPLETED": False,
+        "UNIVERSE": "Kanto Region"
+    },
+    {
+        "NAME": "Venusaur Evolution",
+        "WINS": 0,
+        "REQUIRED": 10,
+        "DEFEAT": "Venusaur",
+        "USE_CARDS": ["Bulbasaur"],
+        "EARN": "Charizard",
+        "COMPLETED": False,
+        "UNIVERSE": "Kanto Region"
+    },        
 ]
