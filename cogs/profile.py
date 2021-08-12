4497d2599d87d1f9af9879671c28a476e03cdb6a
christopher simmons <kadensdad93@gmail.com>
2021-08-12 11:28:37 -0700

    .

--- cogs/profile.py
+++ cogs/profile.py
@@ -238,7 +238,7 @@ class Profile(commands.Cog):
             , colour=000000)
             embedVar.set_thumbnail(url=active_pet['PATH'])
             embedVar.set_image(url=o_card_path)
-            embedVar.set_footer(text=f"EXP Until Next Level: {100 - card_exp}\nRebirth Buff: +{rebirthBonus}", icon_url="https://cdn.discordapp.com/emojis/841486485826961448.gif?v=1")
+            embedVar.set_footer(text=f"EXP Until Next Level: {150 - card_exp}\nRebirth Buff: +{rebirthBonus}", icon_url="https://cdn.discordapp.com/emojis/841486485826961448.gif?v=1")
 
             await ctx.send(embed=embedVar)
         else:
