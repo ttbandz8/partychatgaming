17f9021af18a2e65e4d7df311f13c8de989a109d
RocBambino <83316065+RocBambino@users.noreply.github.com>
2021-09-10 23:00:59 -0400

    updated viewboss and universe

--- cogs/boss.py
+++ cogs/boss.py
@@ -1,3 +1,4 @@
+import textwrap
 import discord
 from discord.ext import commands
 import bot as main
@@ -6,6 +7,7 @@ import classes as data
 import messages as m
 import numpy as np
 import help_commands as h
+import unique_traits as ut
 # Converters
 from discord import User
 from discord import Member
@@ -28,51 +30,88 @@ class Boss(commands.Cog):
 
     @cog_ext.cog_slash(description="View a Boss", guild_ids=main.guild_ids)
     async def viewboss(self, ctx, boss : str):
-        uboss_name = boss
-        uboss = db.queryBoss({'NAME': {"$regex": str(uboss_name), "$options": "i"}})
-        if uboss:
-            uboss_name = uboss['NAME']
-            uboss_show = uboss['UNIVERSE']
-            uboss_title = uboss['TITLE']
-            uboss_arm = uboss['ARM']
-            uboss_desc = uboss['DESCRIPTION'][3]
-            uboss_pic = uboss['PATH']
-            uboss_pet = uboss['PET']
+        try:
+            uboss_name = boss
+            uboss = db.queryBoss({'NAME': {"$regex": str(uboss_name), "$options": "i"}})
+            if uboss:
+                uboss_name = uboss['NAME']
+                uboss_show = uboss['UNIVERSE']
+                uboss_title = uboss['TITLE']
+                uboss_arm = uboss['ARM']
+                uboss_desc = uboss['DESCRIPTION'][3]
+                uboss_pic = uboss['PATH']
+                uboss_pet = uboss['PET']
 
-            arm = db.queryArm({'ARM': uboss_arm})
-            arm_passive = arm['ABILITIES'][0]
-            arm_passive_type = list(arm_passive.keys())[0]
-            arm_passive_value = list(arm_passive.values())[0]
+                arm = db.queryArm({'ARM': uboss_arm})
+                arm_passive = arm['ABILITIES'][0]
+                arm_passive_type = list(arm_passive.keys())[0]
+                arm_passive_value = list(arm_passive.values())[0]
 
-            title = db.queryTitle({'TITLE': uboss_title})
-            title_passive = title['ABILITIES'][0]
-            title_passive_type = list(title_passive.keys())[0]
-            title_passive_value = list(title_passive.values())[0]
+                title = db.queryTitle({'TITLE': uboss_title})
+                title_passive = title['ABILITIES'][0]
+                title_passive_type = list(title_passive.keys())[0]
+                title_passive_value = list(title_passive.values())[0]
+                
+                pet = db.queryPet({'PET': uboss_pet})
+                pet_ability = pet['ABILITIES'][0]
+                pet_ability_name = list(pet_ability.keys())[0]
+                pet_ability_type = list(pet_ability.values())[1]
+                pet_ability_value = list(pet_ability.values())[0]
+
+                traits = ut.traits
+
+                if uboss_show != 'Unbound':
+                    uboss_show_img = db.queryUniverse({'TITLE': uboss_show})['PATH']
+                message= uboss_desc
+                
+                mytrait = {}
+                traitmessage = ''
+                for trait in traits:
+                    if trait['NAME'] == uboss_show:
+                        mytrait = trait
+                    if uboss_show == 'Kanto Region' or uboss_show == 'Johto Region' or uboss_show == 'Kalos Region' or uboss_show == 'Unova Region' or uboss_show == 'Sinnoh Region' or uboss_show == 'Hoenn Region' or uboss_show == 'Galar Region' or uboss_show == 'Alola Region':
+                        if trait['NAME'] == 'Pokemon':
+                            mytrait = trait
+                if mytrait:
+                    traitmessage = f"**{mytrait['EFFECT']}**: {mytrait['TRAIT']}"
+                
+                embedVar = discord.Embed(title=f":japanese_ogre: | {uboss_name}\n:earth_africa: | {uboss_show} Boss", description=textwrap.dedent(f"""
+                **{message}**\n
+     
+                :flower_playing_cards: | **Card** - {uboss_name}\n                                                                           
+                :reminder_ribbon: | **Title** - {uboss_title}: **{title_passive_type}** - {title_passive_value}\n
+                :mechanical_arm: | **Arm** - {uboss_arm}: **{arm_passive_type}** - {arm_passive_value}\n
+                :bird: | **Pet** - {uboss_pet}: **{pet_ability_type}**: {pet_ability_value}\n
+                
+                :infinity: | **Universe Trait** - {traitmessage}
+                """), colour=000000)
+                if uboss_show != "Unbound":
+                    embedVar.set_thumbnail(url=uboss_show_img)
+                embedVar.set_image(url=uboss_pic)
+
+                await ctx.send(embed=embedVar)
+
+
+            else:
+                await ctx.send(m.ARM_DOESNT_EXIST, delete_after=3)
             
-            pet = db.queryPet({'PET': uboss_pet})
-            pet_ability = pet['ABILITIES'][0]
-            pet_ability_name = list(pet_ability.keys())[0]
-            pet_ability_type = list(pet_ability.values())[1]
-            pet_ability_value = list(pet_ability.values())[0]
-
-
-
-            if uboss_show != 'Unbound':
-                uboss_show_img = db.queryUniverse({'TITLE': uboss_show})['PATH']
-            message= uboss_desc
-
-            embedVar = discord.Embed(title=f"{uboss_name}".format(self), description=f"{message}", colour=000000)
-            if uboss_show != "Unbound":
-                embedVar.set_thumbnail(url=uboss_show_img)
-            embedVar.set_image(url=uboss_pic)
-            embedVar.add_field(name="TITLE", value=f"{uboss_title}\nIncreases **{title_passive_type}** by **{title_passive_value}**", inline=True)
-            embedVar.add_field(name="ARM", value=f"{uboss_arm}\nIncreases **{arm_passive_type}** by **{arm_passive_value}**", inline=True)
-            embedVar.add_field(name="PET", value=f"{uboss_pet}\nAbilitiy:**{pet_ability_name}**\nIncreases **{pet_ability_type}** by **{pet_ability_value}**", inline=True)
-
-            await ctx.send(embed=embedVar)
-
-        else:
-            await ctx.send(m.ARM_DOESNT_EXIST, delete_after=3)
+        except Exception as ex:
+            trace = []
+            tb = ex.__traceback__
+            while tb is not None:
+                trace.append({
+                    "filename": tb.tb_frame.f_code.co_filename,
+                    "name": tb.tb_frame.f_code.co_name,
+                    "lineno": tb.tb_lineno
+                })
+                tb = tb.tb_next
+            print(str({
+                'type': type(ex).__name__,
+                'message': str(ex),
+                'trace': trace
+            }))
+            await ctx.send(f"Error when viewing boss. Alert support. Thank you!")
+            return
 
 
 
--- cogs/universe.py
+++ cogs/universe.py
@@ -1,3 +1,4 @@
+import textwrap
 import discord
 from discord.ext import commands
 import bot as main
@@ -5,6 +6,7 @@ import db
 import classes as data
 import messages as m
 import numpy as np
+import unique_traits as ut
 import help_commands as h
 # Converters
 from discord import User
@@ -27,16 +29,79 @@ class Universe(commands.Cog):
         return await main.validate_user(ctx)
 
     @cog_ext.cog_slash(description="View a universe", guild_ids=main.guild_ids)
-    async def viewUniverse(self, ctx, universe: str):
+    async def viewuniverse(self, ctx, universe: str):
         try:
             universe_name = universe
             universe = db.queryUniverse({'TITLE': {"$regex": f"^{universe_name}$", "$options": "i"}})
             universe_name = universe['TITLE']
+            ttitle = "Starter"
+            tarm = "Stock"
+            dtitle = "Reborn"
+            darm = "Reborn Stock"
+            dpet = "Chick"
+            boss = "Bossless"
+            crest = Crest_dict['Unbound']
+            prerec = ""
+            owner = "PCG"
+            traits = ut.traits
             if universe:
                 universe_title= universe['TITLE']
+                fights = len(universe['CROWN_TALES'])
+                crest = Crest_dict[universe_title]
                 universe_image = universe['PATH']
+                ttitle = universe['UTITLE']
+                tarm = universe['UARM']
+                dtitle = universe['DTITLE']
+                darm = universe['DARM']
+                dpet = universe['DPET']
+                boss = universe['UNIVERSE_BOSS']
+                bossmessage = f"*Use /viewboss {boss}*"
+                if boss == "":
+                    bossmessage = f"No {universe_title} Boss available yet!"
+                prerec = universe['PREREQUISITE']
+                
+                prerecmessage = f"Compelete the {prerec} Tale to unlock this Universes!"
+                if prerec == "":
+                    prerec = "Starter Universe"
+                    prerecmessage = "Compelete this Starter Tale to unlock additional Universes!"
+                owner = universe['GUILD']
+                ownermessage = f"{universe_title} is owned by the {owner} Guild!"
+                if owner == "PCG":
+                    owner = "Crest Unclaimed"
+                    ownermessage = "*Complete the /dungeon To Claim this Universe for your Guild!*"
+                   
+                
+                mytrait = {}
+                traitmessage = ''
+                for trait in traits:
+                    if trait['NAME'] == universe_title:
+                        mytrait = trait
+                    if universe_title == 'Kanto Region' or universe_title == 'Johto Region' or universe_title == 'Kalos Region' or universe_title == 'Unova Region' or universe_title == 'Sinnoh Region' or universe_title == 'Hoenn Region' or universe_title == 'Galar Region' or universe_title == 'Alola Region':
+                        if trait['NAME'] == 'Pokemon':
+                            mytrait = trait
+                if mytrait:
+                    traitmessage = f"**{mytrait['EFFECT']}**: {mytrait['TRAIT']}"
+                    
 
-                embedVar = discord.Embed(title=f"{universe_name}".format(self), description=f"Crown Unlimited Universe")
+                embedVar = discord.Embed(title=f":earth_africa: | {universe_title} :crossed_swords: {fights}", description=textwrap.dedent(f"""
+                {crest} | **{ownermessage}**\n
+                
+                :crown: | **Tales Build**\n                                                                               
+                :reminder_ribbon: | **Title** - {ttitle}
+                :mechanical_arm: | **Arm** - {tarm}\n
+
+                :fire: | **Dungeon Build**\n
+                :reminder_ribbon: | **Title** - {dtitle}
+                :mechanical_arm: | **Arm** - {darm}
+                :bird: | **Pet** {dpet}\n
+                
+                :japanese_ogre: | **Universe Boss**
+                :flower_playing_cards: | **Card** - {boss}
+                {bossmessage}\n
+
+                :infinity: | **Universe Trait** - {traitmessage}
+                :lock: | **Prerequisites:** {prerecmessage}
+                """), colour=000000)
                 embedVar.set_image(url=universe_image)
 
                 await ctx.send(embed=embedVar)
@@ -63,3 +128,29 @@ class Universe(commands.Cog):
 
 def setup(bot):
     bot.add_cog(Universe(bot))
+    
+Crest_dict = {'Unbound': ':ideograph_advantage:',
+              'My Hero Academia': ':sparkle:',
+              'League Of Legends': ':u6307:',
+              'Kanto Region': ':chart:',
+              'Naruto': ':u7121:',
+              'Bleach': ':u6709:',
+              'God Of War': ':u7533:',
+              'Chainsawman' : ':accept:',
+              'One Punch Man': ':u55b6:',
+              'Johto Region': ':u6708:',
+              'Black Clover': ':ophiuchus:',
+              'Demon Slayer': ':aries:',
+              'Attack On Titan': ':taurus:',
+              '7ds': ':capricorn:',
+              'Hoenn Region': ':leo:',
+              'Digimon': ':cancer:',
+              'Fate': ':u6e80:',
+              'Solo Leveling': ':u5408:',
+              'Souls': ':sos:',
+              'Dragon Ball Z': ':u5272:',
+              'Sinnoh Region': ':u7981:',
+              'Death Note': ':white_flower:',
+              'Crown Rift Awakening': ':u7a7a:',
+              'Crown Rift Slayers': ':sa:',
+              'Crown Rift Madness': ':loop:'}
