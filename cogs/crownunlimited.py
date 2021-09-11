06efc23403e8d0ace4e993f6d35830671b4418b9
christopher simmons <kadensdad93@gmail.com>
2021-09-10 23:51:36 -0700

    .

--- cogs/crownunlimited.py
+++ cogs/crownunlimited.py
@@ -150,7 +150,7 @@ class CrownUnlimited(commands.Cog):
             """), colour=0xf1c40f)
             embedVar.set_author(name="Enemy Approaches!")
             embedVar.set_thumbnail(url=f"{cards[rand_card]['PATH']}")
-            embedVar.set_footer(text="Use /explore to toggle Explore Mode.", icon_url="https://cdn.discordapp.com/emojis/784402243519905792.gif?v=1")
+            embedVar.set_footer(text="Use /explore to turn off/on these interactions.", icon_url="https://cdn.discordapp.com/emojis/784402243519905792.gif?v=1")
             await message.channel.send(embed=embedVar, components=[random_battle_buttons_action_row])
 
             def check(button_ctx):
