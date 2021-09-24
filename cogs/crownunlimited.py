4d22e00cf2452161b33bc8b9afb6ce1111275f07
christopher simmons <kadensdad93@gmail.com>
2021-09-24 14:49:15 -0700

    fixed persona in battle

--- cogs/crownunlimited.py
+++ cogs/crownunlimited.py
@@ -4573,11 +4573,21 @@ class CrownUnlimited(commands.Cog):
                                                 t_max_health = round(t_max_health - dmg['DMG'])
 
                                             o_stamina = o_stamina - int(dmg['STAMINA_USED'])
+                                            
+                                            if o_universe == "Persona":
+                                                petdmg = damage_cal(o_universe, o_card, o_1, o_attack, o_defense, t_defense, o_vul, o_accuracy, o_stamina, o_enhancer_used, o_health, t_health, t_stamina, o_max_health, t_attack, o_special_move_description, turn_total, ocard_lvl_ap_buff)
+                                                t_health = t_health - petdmg['DMG']
 
-                                            embedVar = discord.Embed(title=f"{o_card.upper()} Summoned :bird: {opet_name}", colour=0xe91e63)
-                                            embedVar.add_field(name=f"{opet_name} used **{opetmove_text}**!", value =f"Enhanced **{opet_type}**")
-                                            embedVar.set_thumbnail(url=opet_image)
-                                            await button_ctx.send(embed=embedVar)
+                                                embedVar = discord.Embed(title=f"**PERSONA!**\n{opet_name} was summoned from {o_card}'s soul dealing **{petdmg['DMG']}** damage!!", colour=0xe91e63)
+                                                embedVar.add_field(name=f"{opet_name} used **{opetmove_text}**!", value =f"Enhanced **{opet_type}**")
+                                                
+                                                embedVar.set_thumbnail(url=opet_image)
+                                                await button_ctx.send(embed=embedVar)
+                                            else:
+                                                embedVar = discord.Embed(title=f"{o_card.upper()} Summoned :bird: {opet_name}", colour=0xe91e63)
+                                                embedVar.add_field(name=f"{opet_name} used **{opetmove_text}**!", value =f"Enhanced **{opet_type}**")
+                                                embedVar.set_thumbnail(url=opet_image)
+                                                await button_ctx.send(embed=embedVar)
                                             turn=0
                                         else:
                                             await button_ctx.send(f"{opet_name} needs a turn to rest...")
@@ -5287,10 +5297,22 @@ class CrownUnlimited(commands.Cog):
                                                     o_max_health = round(o_max_health - dmg['DMG'])
                                                 t_stamina = t_stamina - int(dmg['STAMINA_USED'])
 
-                                                embedVar = discord.Embed(title=f"{t_card.upper()} Summoned :bird: {tpet_name}", colour=0xe91e63)
-                                                embedVar.add_field(name=f"{tpet_name} used **{tpetmove_text}**!", value =f"Enhanced **{tpet_type}**")
-                                                embedVar.set_thumbnail(url=tpet_image)
-                                                await button_ctx.send(embed=embedVar)
+                                                if t_universe == "Persona":
+                                                    petdmg = damage_cal(t_universe, t_card, t_1, t_attack, t_defense, c_defense, t_vul, t_accuracy, t_stamina, t_enhancer_used, t_health, c_health, c_stamina, t_max_health, c_attack, t_special_move_description, turn_total, tcard_lvl_ap_buff)
+
+                                                    o_health = o_health - petdmg['DMG']
+
+                                                    embedVar = discord.Embed(title=f"**PERSONA!**\n{tpet_name} was summoned from {t_card}'s soul dealing **{petdmg['DMG']}** damage!!", colour=0xe91e63)
+                                                    embedVar.add_field(name=f"{tpet_name} used **{tpetmove_text}**!", value =f"Enhanced **{tpet_type}**")
+                                                    
+                                                    embedVar.set_thumbnail(url=tpet_image)
+                                                    await button_ctx.send(embed=embedVar)
+                                                else:
+                                                    embedVar = discord.Embed(title=f"{t_card.upper()} Summoned :bird: {tpet_name}", colour=0xe91e63)
+                                                    embedVar.add_field(name=f"{tpet_name} used **{tpetmove_text}**!", value =f"Enhanced **{tpet_type}**")
+                                                    embedVar.set_thumbnail(url=tpet_image)
+                                                    await button_ctx.send(embed=embedVar)
+                                                    
                                                 turn=1
                                             else:
                                                 await button_ctx.send(f"{tpet_name} needs a turn to rest...")
@@ -14707,7 +14729,7 @@ async def battle_commands(self, ctx, mode, universe, selected_universe, complete
                                                     embedVar = discord.Embed(title=f"{t_card.upper()} Summoned :bird: {tpet_name}", colour=0xe91e63)
                                                     embedVar.add_field(name=f"{tpet_name} used **{tpetmove_text}**!", value =f"Enhanced **{tpet_type}**")
                                                     embedVar.set_thumbnail(url=tpet_image)
-                                                    await private_channel.send(embed=embedVar)
+                                                    await button_ctx.send(embed=embedVar)
                                             
                                             if t_universe == "Persona":
                                                 petdmg = damage_cal(t_universe, t_card, t_1, t_attack, t_defense, c_defense, t_vul, t_accuracy, t_stamina, t_enhancer_used, t_health, c_health, c_stamina, t_max_health, c_attack, t_special_move_description, turn_total, tcard_lvl_ap_buff)
