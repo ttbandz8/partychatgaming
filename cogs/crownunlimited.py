e55b6db88d573df9d5dff790c47f44b27a332d95
christopher simmons <kadensdad93@gmail.com>
2022-01-02 00:40:35 -0800

    updated explore encounter

--- cogs/crownunlimited.py
+++ cogs/crownunlimited.py
@@ -40,7 +40,7 @@ from pilmoji import Pilmoji
 class CrownUnlimited(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self._cd = commands.CooldownMapping.from_cooldown(1, 10,
+        self._cd = commands.CooldownMapping.from_cooldown(1, 1500,
                                                           commands.BucketType.member)  # Change accordingly. Currently every 8 minutes (3600 seconds == 60 minutes)
 
     co_op_modes = ['CTales', 'DTales', 'CDungeon', 'DDungeon']
