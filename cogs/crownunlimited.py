db91397383f436df5d811319d401ec6d4b9a9431
RocBambino <83316065+RocBambino@users.noreply.github.com>
2022-02-15 12:49:18 -0500

    hit chances

--- cogs/crownunlimited.py
+++ cogs/crownunlimited.py
@@ -2890,9 +2890,9 @@ def damage_cal(universe, card, ability, attack, defense, op_defense, stamina, en
             true_dmg = (random.randint(int(low), int(high))) + 25
             message = ""
 
-            miss_hit = 3  # Miss
+            miss_hit = 2  # Miss
             low_hit = 7  # Lower Damage
-            med_hit = 11  # Medium Damage
+            med_hit = 13  # Medium Damage
             standard_hit = 19  # Standard Damage
             high_hit = 20  # Crit Hit
             hit_roll = random.randint(3, 20)
