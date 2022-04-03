e07823261f89cd48751ef24b4c8a5412583943e7
christopher simmons <kadensdad93@gmail.com>
2022-04-03 10:14:26 -0700

    /

--- bot.py
+++ bot.py
@@ -123,9 +123,8 @@ async def on_ready():
         print(server.name)
 
 token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijk1NTcwNDkwMzE5ODcxMTgwOCIsImJvdCI6dHJ1ZSwiaWF0IjoxNjQ5MDAyNDYzfQ.OGIjvyo2mlOrfZTTLoyIODNKzvk_7o-0tP5zwA31JsE'  # set this to your DBL token
-bot.topggpy  = topgg.WebhookManager(bot).dbl_webhook("/dblwebhook", "KqXJUqmipftsrmJREC-XjYqJkM2SOAM1Gw3uscD9SU8V191Q45EikQTjNhJiwq73_op2")
-
-        #.DBLClient(self.bot, self.token, webhook_path='/dblwebhook', webhook_auth='KqXJUqmipftsrmJREC-XjYqJkM2SOAM1Gw3uscD9SU8V191Q45EikQTjNhJiwq73_op2', webhook_port=5000)
+bot.topgg_webhook = topgg.WebhookManager(bot).dbl_webhook("/dblwebhook", "KqXJUqmipftsrmJREC-XjYqJkM2SOAM1Gw3uscD9SU8V191Q45EikQTjNhJiwq73_op2")
+bot.topgg_webhook.run(5000)
 
 @bot.event
 async def on_ready():
