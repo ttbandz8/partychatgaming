12faeeb6220cb9aa3f5c883a5469a59601e409f5
christopher simmons <kadensdad93@gmail.com>
2022-04-03 22:22:17 -0700

    .

--- bot.py
+++ bot.py
@@ -103,7 +103,6 @@ async def help(ctx: SlashContext):
 
    await Paginator(bot=bot, ctx=ctx, pages=embeds, timeout=60).run()
 
-
 async def validate_user(ctx):
    query = {'DID': str(ctx.author.id)}
    valid = db.queryUser(query)
@@ -114,30 +113,35 @@ async def validate_user(ctx):
       msg = await ctx.send(m.USER_NOT_REGISTERED, delete_after=5)
       return False
 
-# bot.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijk1NTcwNDkwMzE5ODcxMTgwOCIsImJvdCI6dHJ1ZSwiaWF0IjoxNjQ5MDAyNDYzfQ.OGIjvyo2mlOrfZTTLoyIODNKzvk_7o-0tP5zwA31JsE'  # set this to your DBL token
-# bot.topggpy = topgg.DBLClient(bot, bot.token)
-topgg_webhook = topgg.WebhookManager(client).dbl_webhook("/dblwebhook", "KqXJUqmipftsrmJREC-XjYqJkM2SOAM1Gw3uscD9SU8V191Q45EikQTjNhJiwq73_op2")
-topgg_webhook.run(5000)
 
-@client.event
+
+bot.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijk1NTcwNDkwMzE5ODcxMTgwOCIsImJvdCI6dHJ1ZSwiaWF0IjoxNjQ5MDAyNDYzfQ.OGIjvyo2mlOrfZTTLoyIODNKzvk_7o-0tP5zwA31JsE'  # set this to your DBL token
+# bot.topggpy = topgg.DBLClient(bot, bot.token)
+
+@bot.event
 async def on_ready():
    print('Bot is ready!')
-   for server in bot.guilds:
-        print(server.name)
+
+   try:
+      topgg_webhook = topgg.WebhookManager(bot).dbl_webhook("/dblwebhook", "KqXJUqmipftsrmJREC-XjYqJkM2SOAM1Gw3uscD9SU8V191Q45EikQTjNhJiwq73_op2").run(5000)
+   except Exception as e:
+      print(e)
+   # for server in bot.guilds:
+   #      print(server.name)
 
 
-@client.event
-async def on_dbl_vote(data):
+@bot.event
+async def on_dbl_vote(ctx, data):
     print("HELLO WORLD VOTE")
     if data["type"] == "test":
         # this is roughly equivalent to
         # return await on_dbl_test(data) in this case
-        return client.dispatch('dbl_test', data)
+        return bot.dispatch('dbl_test', data)
 
     print(f"Received a vote:\n{data}")
 
-@client.event
-async def on_dbl_test(data):
+@bot.event
+async def on_dbl_test(ctx, data):
    print("HELLO WORLD TEST")
    print(f"Received a test vote:\n{data}")
 
